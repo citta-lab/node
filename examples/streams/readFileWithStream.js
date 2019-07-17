ea729a18d9f6db80203528bd6b7a8d1a21aa40cf
Mahesh <mahesh2684@gmail.com>
2019-07-16 21:24:08 -0400

    example

--- examples/streams/readFileWithStream.js
+++ examples/streams/readFileWithStream.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 "use strict";
 let path = require('path');
-var file_args = require("minimist")(process.argv.slice(2), {string: ["file"]});
+var fs = require('fs');
 
 function readData(){
+    const args = require("minimist")(process.argv.slice(2), {
+        string: ["file"]
+    });
     const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
     let stream = fs.createReadStream(path.join(BASE_PATH, args.file));
     processFileAsStream(stream);
@@ -14,4 +17,12 @@ function processFileAsStream(streamData){
     streamData.pipe(targetStream);
 }
 
-readData();
+readData(); // Hello World
+
+
+/**
+ * Instructions to Execute the Script.
+ * FileName: prcoessFile.js
+ * DataFile: dataFile.js ( contains 'Hello World' )
+ * Execute Command: `./processFile.js --file=dataFile.js`
+ */
